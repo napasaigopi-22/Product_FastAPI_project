cd6d9d90d0b07ff14f804c8232cb62fd00301cd1
unknown <cb.sc.p2cse24008@cb.students.amrita.edu>
2026-06-24 17:55:54 +0530

    msg

--- Random_function_code.py
+++ Random_function_code.py
@@ -1,4 +1,12 @@
 def printing_dogs_name(first_name, last_name):
     print_sentence= first_name + ":" + last_name
     return print_sentence
-print("My pet name is","Daschund")
+print(printing_dogs_name("My pet name is","Daschund"))
+
+
+# with the help of FASTAPI
+def printing_dogs_names(firstname: str, lastname: str):
+    print_dogs_sentence = firstname.title() +":" + lastname.upper()
+    return print_dogs_sentence
+
+print(printing_dogs_names("My pet name is","Daschund"))
